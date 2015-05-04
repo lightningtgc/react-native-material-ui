7d773e1690ed993f2da32dfc9e6baf7c962f94e5
gctang <lightningtgc@gmail.com>
2015-05-04 13:46:45 +0800

    Create button.js

---
+++ src/button.js
@@ -0,0 +1,16 @@
+/*
+* Material design button style for react native 
+* By gctang
+*/
+
+'use strict';
+
+var React = require('react-native');
+
+var {
+  StyleSheet,
+} = React;
+
+module.exports = StyleSheet.create({
+
+});
